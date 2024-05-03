17d99f49fd5701d97500366078c207f3b46a4d06
YoussefAhmed488 <youssefahmed4869@gmail.com>
2024-05-03 21:34:10 +0300

    data is splited
    i did split the data

--- GuideMe/filereader.cpp
+++ GuideMe/filereader.cpp
@@ -10,7 +10,8 @@
 #include <QCoreApplication>
 #include <QString>
 #include <QDebug>
-
+#include <sstream>
+#include<string>
 using namespace std;
 
 FileReader::FileReader()
@@ -24,15 +25,42 @@ void FileReader::readFile()
     string content;
 
     if (file.is_open()) { // Check if file is open
-        std::string line;
-        while (std::getline(file, line)) { // Read the file line by line
-            content += line + "\n"; // Append each line to the content string
+        string line;
+        getline(file,line);
+        int num;
+        num=stoi(line);
+        cout<<num<<endl;
+        while (getline(file, line)) { // Read the file line by line
+
+            istringstream str(line);
+            string city1, city2, trans_info,ignore;
+            cout << line << endl; // Print the current line
+
+            //Extract cities and split
+            getline(str, city1, '-');
+            getline(str, ignore, ' ');
+            getline(str, city2, ' ');
+
+            // Removing leading and trailing spaces
+            city1.erase(city1.find_last_not_of(" ") + 1);
+            //city2.erase(0, city2.find_first_not_of(" "));
+            //cout << "<"<< ignore << ">";
+            cout << city1 << " - " << city2<<" ";
+
+            while (str >> trans_info) {
+                string s = trans_info;
+                int cost;
+                str >> cost;
+                cout << trans_info << " " << cost<<" ";
+            }
+            cout << endl;
         }
-        file.close(); // Close the file
-    } else {
+    }
+    else {
         std::cerr << "Unable to open file"; // Error if unable to open file
         //return 1;
     }
+    file.close(); // Close the file
 
     std::cout << "Content of the file:\n" << content << std::endl;
 }
