9b27b743c29c6f6fd2d94ffd0fec60aeeb74b84d
Elsheikh <youssefelsheikh506@gmail.com>
2024-05-10 02:38:22 +0300

    final toutch

--- GuideMe/graph.cpp
+++ GuideMe/graph.cpp
@@ -51,7 +51,7 @@ void Graph::getAllPaths(string s, string d)
     cout<< endl;
     //Call the recursive function to print all paths
     findAllPathsDFS("", s, d, visited, paths,path, path_index);
-    //dijkstra("Giza","BeniSuef");
+    dijkstra("Giza","BeniSuef");
 }
 
 void Graph::findAllPathsBFS(string source, string dest) {
@@ -151,6 +151,7 @@ void Graph::findAllPathsDFS(string prevNode,string u, string d, map<string, bool
 
 void Graph::dijkstra(string start,string end)
 {
+    bool found = 0;
     string node;
     priority_queue<pair<float,string>>pq;
     unordered_map<string,pair<string,string>>parent;
@@ -183,10 +184,15 @@ void Graph::dijkstra(string start,string end)
                 //Update The Old Cost By The Better One
                 cost[child.endNode] = cur_cost + child.cost;
                 pq.push({-cost[child.endNode],child.endNode});
+                if(end == child.endNode)
+                    found = 1;
+
             }
         }
     }
-    while (start != end) {
+    if(!found)
+        cout <<"NOT Found \n";
+    while (start != end and found) {
         cout <<"From "<< end <<" To "<< parent[end].first<<" by "<<parent[end].second<<" ";
         end = parent[end].first;
     }
